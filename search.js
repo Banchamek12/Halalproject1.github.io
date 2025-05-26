c8918d97f07d407e69ee0ad77f66d39dbdf0123d
Banchamek12 <148534453+Banchamek12@users.noreply.github.com>
2025-05-26 22:56:32 +0700

    Delete search.js

--- search.js
+++
@@ -1,5 +0,0 @@
-const allowedWords = ["hello", "world", "language", "english"];
-card.innerHTML = `<strong>${word}</strong> → ${translation}`;
-const languageName = document.getElementById('languageSelect').options[document.getElementById('languageSelect').selectedIndex].text;
-card.innerHTML = `<strong>${word}</strong> in ${languageName} → ${translation}`;
-
